6eddff78340ae652f849609db118aed8ba244ef7
Xiaojian Chen <lzokchen@qq.com>
2020-10-23 13:08:12 -0700

    refactor the codes that are using riotAPI

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,6 @@ require('honeycomb-beeline')({
 })
 
 import assert from 'assert'
-import axios from 'axios' // import axios for http requests
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { json, raw, RequestHandler, static as expressStatic } from 'express'
@@ -21,21 +20,16 @@ import { checkEqual, Unpromise } from '../../common/src/util'
 import { Config } from './config'
 import { migrate } from './db/migrate'
 import { initORM } from './db/sql'
-import { RecentMatch } from './entities/RecentMatch'
 import { Session } from './entities/Session'
-import { Summoner } from './entities/Summoner'
 import { User } from './entities/User'
 import { getSchema, graphqlRoot, pubsub } from './graphql/api'
 import { ConnectionManager } from './graphql/ConnectionManager'
 import { expressLambdaProxy } from './lambda/handler'
 import { renderApp } from './render'
+import { RiotAPI } from './riotAPI'
 
-const riotToken = "{Your own Token}"
 
-// create axios instance with customized baseURL
-const instance = axios.create({
-  baseURL: 'https://na1.api.riotgames.com/lol/'
-})
+
 
 const server = new GraphQLServer({
   typeDefs: getSchema(),
@@ -54,121 +48,14 @@ const asyncRoute = (fn: RequestHandler) => (...args: Parameters<RequestHandler>)
 server.express.get('/', (req, res) => {
   console.log('GET /')
   res.redirect('/app')
-  // sample instance http requests call with riot token in header
-  instance({
-    method: 'get',
-    url: '/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5',
-    headers:
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
-      "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,it-IT;q=0.5,it;q=0.4",
-      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-      "Origin": "https://developer.riotgames.com",
-      "X-Riot-Token": riotToken
-    }
-  })
-  .then(async function (response) {
 
-    console.log("Parsing and Saving challengerData")
-    const jsonObj = JSON.parse(JSON.stringify(response.data))
-    const entries = jsonObj.entries
-    entries.forEach(async (element: any) => {
-      //console.log(element.summonerName)
-
-      //check whether the summoner is exist in the database
-      var summoner = await(Summoner.findOne({ where: { summonerId : element.summonerId}}))
-      if (!summoner){ //if not create a new one
-        summoner = new Summoner()
-        summoner.summonerId = element.summonerId
-      }
-      summoner.summonerName = element.summonerName
-      summoner.leaguePoints = element.leaguePoints
-      summoner.isTop300 = true //uses to differentiate Top 300 player that we got from the Ranking and other player that we got from normal searching
-      summoner.rank = element.rank
-      summoner.wins = element.wins
-      summoner.losses = element.losses
-      summoner.veteran = element.veteran
-      summoner.inactive = element.inactive
-      summoner.freshBlood = element.freshBlood
-      summoner.hotStreak = element.hotStreak
-
-      Summoner.save(summoner)//.then(s => console.log('saved summoner: ' + s.summonerName))
-    });
-    console.log("ChallengerData is saved")
-  });
-  // For individual player search, first find accountId by summonerName
-  var searchName = "rovex1"
-  instance({
-    method: 'get',
-    url: '/summoner/v4/summoners/by-name/' + searchName, // can be any player name, i.e. /summoner/v4/summoners/by-name/{playerName}
-    headers:
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
-      "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,it-IT;q=0.5,it;q=0.4",
-      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-      "Origin": "https://developer.riotgames.com",
-      "X-Riot-Token": riotToken
-    }
-  })
-  .then(async function (response) {
-    console.log("Search summoner by name success: " + searchName)
-    const summonerByName = JSON.parse(JSON.stringify(response.data))
-    var summoner = await(Summoner.findOne({ where: { summonerId : summonerByName.id}}))
-
-    if (!summoner){
-      summoner = new Summoner()
-      summoner.summonerId = summonerByName.id
-    }
-    summoner.accountId = summonerByName.accountId
-    summoner.summonerName = summonerByName.name
-    summoner.profileIconId = summonerByName.profileIconId
-    summoner.summonerLevel = summonerByName.summonerLevel
-
-    Summoner.save(summoner)
-
-    const playerAccountID = summoner.accountId
-    const playerName = summoner.summonerName
-    instance({
-      method: 'get',
-      url: '/match/v4/matchlists/by-account/' + playerAccountID + '?endIndex=10', // can be any accountId, i.e. /match/v4/matchlists/by-account/{accountId}
-      headers:
-      {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
-        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,it-IT;q=0.5,it;q=0.4",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": riotToken
-      }
-    })
-    .then(function (response) {
-      console.log("Saving player: \""  + "\" recent 10 matches")
-      const parsed = JSON.parse(JSON.stringify(response.data))
-      const recentMatches = parsed.matches
-      recentMatches.forEach(async (element : any) => {
-        var recentMatch = await(RecentMatch.findOne({ where: { accountId : playerAccountID, gameId : element.gameId}}))
-        if (!recentMatch){
-          recentMatch = new RecentMatch()
-          recentMatch.accountId = playerAccountID
-          recentMatch.summonerName = playerName
-          recentMatch.platformId = element.platformId
-          recentMatch.gameId = element.gameId
-          recentMatch.champion = element.champion
-          recentMatch.queue = element.queue
-          recentMatch.season = element.season
-          recentMatch.timestamp = element.timestamp
-          recentMatch.role = element.role
-          recentMatch.lane = element.lane
-          RecentMatch.save(recentMatch)
-        }
-      });
-      console.log("Recent 10 matches of player: \"" + playerName + "\" are saved")
-    });
-  })
 })
 
 server.express.get('/app/*', (req, res) => {
   console.log('GET /app')
   renderApp(req, res)
+  var riotAPI = new RiotAPI("RGAPI-987593b7-725e-40db-8320-5f755118b574")
+  riotAPI.updateChallengerData()
 })
 
 server.express.get('/app/search', (req, res) => {
